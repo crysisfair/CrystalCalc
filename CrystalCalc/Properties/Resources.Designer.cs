8000bb4cd7493ff9cbbdf5cbf6bd8fc87e96f181
crysisfair <crysisfair@qq.com>
2017-09-22 23:47:31 +0800

    Change name to WuliCalc

--- CrystalCalc/Properties/Resources.Designer.cs
+++ CrystalCalc/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CrystalCalc.Properties {
+namespace WuliCalc.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CrystalCalc.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CrystalCalc.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WuliCalc.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
